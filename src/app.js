d45b1522dfb50056879c861ce1636892b526480d
developersworkspace <developersworkspace@gmail.com>
2018-03-05 08:49:49 +0200

    update

---
+++ src/app.js
@@ -0,0 +1,91 @@
+const argv = require('yargs')
+.option('pattern', {
+    alias: 'r',
+    description: 'Regex pattern',
+    type: 'string',
+})
+.option('ports', {
+    alias: 'p',
+    description: 'Comma-separated list of ports',
+    type: 'string',
+})
+.option('servers', {
+    alias: 's',
+    description: 'Comma-separated list of server IP Addresses/Hostnames',
+    type: 'string',
+})
+.argv;
+
+const chalk = require('chalk');
+const redis = require("redis");
+
+(async () => {
+
+    for (const server of argv.servers.split(',')) {
+
+        for (const port of argv.ports.toString().split(',')) {
+
+            try {
+                var client = redis.createClient(port, server, {
+                    retry_strategy: (options) => {
+                        return undefined;
+                    },
+                });
+
+                const keys = await listKeys(client, argv.pattern);
+
+                console.log(chalk.blue(`Found ${keys.length} on ${server}:${port}.`));
+
+                let failedCount = 0;
+
+                for (const key of keys) {
+                    try {
+                        if (!argv.dry) {
+                            await deleteKey(key);
+                        }
+                    } catch (err) {
+                        failedCount++;
+                    }
+                }
+
+                if (failedCount) {
+                    console.log(chalk.red(`Failed to delete ${failedCount} keys.`));
+                } else {
+                    console.log(chalk.green(`Successfully deleted ${keys.length} keys.`));
+                }
+
+            } catch (err) {
+                console.log(chalk.red(`Failed to connect to ${server}:${port}.`));
+            }
+            finally {
+                client.end(false);
+            }
+        }
+    }
+})();
+
+async function listKeys(client, pattern) {
+    return new Promise((resolve, reject) => {
+        client.keys(pattern, (err, keys) => {
+            if (err) {
+                reject(err);
+                return;
+            }
+
+            resolve(keys);
+        });
+    });
+}
+
+async function deleteKey(client, key) {
+    return new Promise((resolve, reject) => {
+        client.del(key, (err) => {
+            if (err) {
+                reject(err);
+                return;
+            }
+
+            resolve();
+        });
+    });
+}
